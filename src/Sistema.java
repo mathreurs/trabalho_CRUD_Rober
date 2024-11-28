7bbdf06c0b0d8c914ea2690784ac5b00177198e9
Matheus Alves Passos <136806259+mathreurs@users.noreply.github.com>
2024-11-28 14:55:09 -0300

    Update Sistema.java

--- src/Sistema.java
+++ src/Sistema.java
@@ -2,12 +2,13 @@
 import Locadora.Adm;
 import Locadora.Locadora;
 
+import Login.Login;
 import java.util.*;
 
 public class Sistema {
     public String cpfAtual; // Armazena o nome do usuário atualmente logado no sistema
 
-    // Método para o sistema administrativo
+    // Metodo para o sistema administrativo
     public void sistemAdm(){
         // Cria uma instância da classe Adm para gerenciar filmes
         Adm adm = new Adm();
@@ -20,7 +21,7 @@ public class Sistema {
             Scanner inp = new Scanner(System.in);
 
             // Exibe o menu de opções para o administrador
-            System.out.println("[1] Adicionar novo filme \n[2] Remover um filme\n[3] Alugar um filme\n[4] Devolver um filme\n[5] Ver os filmes no catálogo\n[6] Procurar um filme\n[7] alterar dados de um filme\n[8] remover um usuário \n[10] Deslogar do sistema");
+            System.out.println("[1] Adicionar novo filme \n[2] Remover um filme\n[3] Alugar um filme\n[4] Devolver um filme\n[5] Ver os filmes no catálogo\n[6] Procurar um filme\n[7] alterar dados de um filme\n[8] remover um usuário\n[9] ver os usuários do sistema \n[10] Deslogar do sistema");
             int opt = inp.nextInt(); // Lê a opção escolhida pelo administrador
             inp.nextLine(); // Limpa o buffer do scanner
 
@@ -42,7 +43,7 @@ public class Sistema {
                         System.out.println("Ano de lançamento: ");
                         int anoLancamento = inp.nextInt();
 
-                        // Chama o método para adicionar o filme ao catálogo da classe Adm
+                        // Chama o metodo para adicionar o filme ao catálogo da classe Adm
                         adm.addFilme(categoria, anoLancamento, estudio, diretor, nomeAdd, codigo, false);
                     }catch(Exception e){
                         System.out.println("Valor invalidos");
@@ -53,41 +54,46 @@ public class Sistema {
                     // Remover filme do sistema
                     System.out.println("Digite o nome do filme que deseja remover: ");
                     String nomeRemove = inp.nextLine();
-                    adm.removerFilme(nomeRemove); // Chama o método de remoção de filme
+                    adm.removerFilme(nomeRemove); // Chama o metodo de remoção de filme
                     break;
 
                 case 3:
                     // Alugar um filme do sistema
                     System.out.println("Digite o nome do filme que deseja alugar: ");
                     String nomeAlugar = inp.nextLine();
-                    adm.alugarFilme(nomeAlugar); // Chama o método de aluguel de filme
+                    adm.alugarFilme(nomeAlugar); // Chama o metodo de aluguel de filme
                     break;
 
                 case 4:
                     // Devolver um filme alugado
                     System.out.println("Digite o nome do filme que deseja devolver: ");
                     String nomeDevolv = inp.nextLine();
-                    adm.devolverFilme(nomeDevolv); // Chama o método de devolução de filme
+                    adm.devolverFilme(nomeDevolv); // Chama o metodo de devolução de filme
                     break;
 
                 case 5:
                     // Mostrar todos os filmes disponíveis no catálogo
-                    adm.mostrarFilmes(); // Chama o método para exibir filmes
+                    adm.mostrarFilmes(); // Chama o metodo para exibir filmes
                     break;
 
                 case 6:
                     // Procurar filme específico no catálogo
-                    adm.procurarFilme(); // Chama o método de busca de filme
+                    adm.procurarFilme(); // Chama o metodo de busca de filme
                     break;
 
                 case 7:
                     // Alterar dados de um filme
-                    adm.alterarFilme(); // Chama o método para alterar informações de um filme
+                    adm.alterarFilme(); // Chama o metodo para alterar informações de um filme
                     break;
 
                 case 8:
                     // Remover um usuário do sistema
                     adm.removerUsuario(cpfAtual); // Remove o usuário atual do sistema
+                    break;
+
+                case 9:
+                    Login log = new Login();
+                    log.mostrarUsuarios(); // mostra os usuarios que foram cadastrados no sistema
                     break;
 
                 case 10:
@@ -99,7 +105,7 @@ public class Sistema {
         }
     }
 
-    // Método para o sistema de usuário
+    // Metodo para o sistema de usuário
     public void sistemUsuario(){
         // Instancia a classe Locadora para gerenciar ações de usuário comum
         Locadora loc = new Locadora();
@@ -122,14 +128,14 @@ public class Sistema {
                     // Alugar um filme
                     System.out.println("Digite o nome do filme que deseja alugar: ");
                     String nomeAlug = inp.nextLine();
-                    loc.alugarFilme(nomeAlug); // Chama o método de aluguel de filme
+                    loc.alugarFilme(nomeAlug); // Chama o metodo de aluguel de filme
                     break;
 
                 case 2:
                     // Devolver um filme alugado
                     System.out.println("Digite o nome do filme que deseja devolver: ");
                     String nomeDevolv = inp.nextLine();
-                    loc.devolverFilme(nomeDevolv); // Chama o método de devolução de filme
+                    loc.devolverFilme(nomeDevolv); // Chama o metodo de devolução de filme
                     break;
 
                 case 3:
@@ -139,7 +145,7 @@ public class Sistema {
 
                 case 4:
                     // Procurar um filme específico
-                    loc.procurarFilme(); // Chama o método de busca de filme
+                    loc.procurarFilme(); // Chama o metodo de busca de filme
                     break;
 
                 case 5:
